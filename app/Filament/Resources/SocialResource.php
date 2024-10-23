463ba17f26633f3e8e1e0269128e5b68662c7b08
ronaldiputra09 <emailnyaronal@gmail.com>
2024-10-23 09:01:53 +0700

    feat : add socials

---
+++ app/Filament/Resources/SocialResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SocialResource\Pages;
+use App\Models\Social;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class SocialResource extends Resource
+{
+    protected static ?string $model = Social::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Name')
+                    ->required()
+                    ->placeholder('Facebook'),
+                TextInput::make('link')
+                    ->label('URL')
+                    ->required()
+                    ->placeholder('https://www.facebook.com/username'),
+                FileUpload::make('icon')
+                    ->label('Icon')
+                    ->imageEditor()
+                    ->imageCropAspectRatio("1:1")
+                    ->directory('socials')
+                    ->required(),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('icon')
+                    ->label('Icon'),
+                TextColumn::make('name')
+                    ->label('Name'),
+                TextColumn::make('link')
+                    ->label('URL'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSocials::route('/'),
+            'create' => Pages\CreateSocial::route('/create'),
+            'edit' => Pages\EditSocial::route('/{record}/edit'),
+        ];
+    }
+}
